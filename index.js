f3fc8e7b92374613f520afac90a9775b688f130d
Konrad <konrad.babowicz21@gmail.com>
2023-07-10 21:02:04 +0100

    commit

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ function createGrid()  {
           cell.classList.add('square');
           
           // Add any desired CSS styles to the cell
-          cell.style.width = "30px";
-          cell.style.height = "30px";
+          cell.style.width = "50px";
+          cell.style.height = "50px";
           cell.style.border = "1px solid black";
           cell.style.display = "flex";
       
@@ -30,10 +30,18 @@ function createGrid()  {
 // Being able to declare all of the squares within the grid
 const hoverSquare = document.getElementsByClassName('.square');
 
+
+
+for (let i = 0; i < hoverSquare.length; i++) {
+
+    hoverSquare.addEventListener('mouseenter' , function(){
+        hoverSquare.style.backgroundColor = "black";
+    })
+}
+
+
 //Creating a function to hover over the square
-hoverSquare.addEventListener("mouseenter" , function() {
-    hoverSquare.style.backgroundColor = "black";
-});
+
 
 //create a function for hovering over.
 // Implement into into the function createGrid
